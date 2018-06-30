4cb3917cb888bc25c2eb4087b4a46bc7e976e4f8
Callum Kerr <callum@clov3r.io>
2018-06-29 22:52:19 -0600

    2 hour mark - user story 1 tests not yet passing

--- lib/bank_ocr.rb
+++ lib/bank_ocr.rb
@@ -1,3 +1,39 @@
 module BankOcr
-  # ...
+  def readEntriesFromFile( file )
+    # chunks a file into entries of 4 lines each
+    entries = [];
+
+    file.split("\n")
+      .each_slice(4) { |entry| entries.push entry.take( 3 ) }
+
+    parseDigits =  entries.map { |entry| separateDigits( entry ) }
+    puts "parseDigits"
+    p parseDigits
+    parseDigits
+  end
+
+
+  def separateDigits( entryLines )
+    puts "entryLines"
+    p entryLines
+    digits = entryLines
+      .map { |line; split_line|
+        split_line = []
+        # puts "line"
+        # p line
+        line.split('').each_slice( 3 ) { | digitLine |
+          split_line.push( digitLine.join ) }
+        # puts "split_line"
+        # p split_line
+        split_line }
+      .transpose()
+
+    puts "separateDigits"
+    p digits
+    # return digits
+  end
+
+  def matchDigit( digit )
+    digit.join('\n')
+  end
 end
--- spec/bank_ocr_spec.rb
+++ spec/bank_ocr_spec.rb
@@ -1,9 +1,32 @@
+FIXTURE_DIR = "/Users/clov3r/Dev/bank_ocr_kata/fixtures/"
+
+class BankOcrTestClient
+  include BankOcr
+end
+
 describe BankOcr do
+  @bankOcr = nil
+  before(:all) do
+    @bankOcr = BankOcrTestClient.new
+    @entries =
+      @bankOcr.readEntriesFromFile( fixture( 'use_case_1_in' ) )
+  end
+
   it "is a module" do
     expect(BankOcr).to be_a Module
   end
 
-  it "loads fixtures" do
-    expect(fixture("use_case_1")).to match(/123456789\n\z/)
+  it "splits a file into entries" do
+            expect( @entries ).to be_an Array
+     expect( @entries.length ).to be 11
+  end
+
+  it "parses an entry into a string of digits" do
+
+    (0..9).each { |entry|
+      expect( @entries[entry] )
+        .to eq ( (1..9).collect{ entry }.join ) }
+
+    expect( @entries[10] ).to eq ( "123456789")
   end
 end
