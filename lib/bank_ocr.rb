58bf0d866d46641aaaede25aa539891865c4df2d
Callum Kerr <callum@clov3r.io>
2018-06-29 23:47:05 -0600

    Tests passing for user story 1

--- lib/bank_ocr.rb
+++ lib/bank_ocr.rb
@@ -1,39 +1,75 @@
-module BankOcr
-  def readEntriesFromFile( file )
-    # chunks a file into entries of 4 lines each
-    entries = [];
+NumbersAs3x3Matrix =
+[ " _ " +
+  "| |" +
+  "|_|" ,
+  "   " +
+  "  |" +
+  "  |" ,
+  " _ " +
+  " _|" +
+  "|_ " ,
+  " _ " +
+  " _|" +
+  " _|" ,
+  "   " +
+  "|_|" +
+  "  |" ,
+  " _ " +
+  "|_ " +
+  " _|" ,
+  " _ " +
+  "|_ " +
+  "|_|" ,
+  " _ " +
+  "  |" +
+  "  |" ,
+  " _ " +
+  "|_|" +
+  "|_|" ,
+  " _ " +
+  "|_|" +
+  " _|" ]
 
-    file.split("\n")
-      .each_slice(4) { |entry| entries.push entry.take( 3 ) }
+LookupNumberFor = Hash[ NumbersAs3x3Matrix.zip ( 0..9 ) ]
 
-    parseDigits =  entries.map { |entry| separateDigits( entry ) }
-    puts "parseDigits"
-    p parseDigits
-    parseDigits
+class BankOcr
+  def initialize( file )
+    @file = file
   end
 
+  # chunks a file into entries of 4 lines each
+  def rawEntries
+    @entries = [];
 
-  def separateDigits( entryLines )
-    puts "entryLines"
-    p entryLines
-    digits = entryLines
-      .map { |line; split_line|
-        split_line = []
-        # puts "line"
-        # p line
-        line.split('').each_slice( 3 ) { | digitLine |
-          split_line.push( digitLine.join ) }
-        # puts "split_line"
-        # p split_line
-        split_line }
-      .transpose()
+    @file.split("\n")
+      .each_slice(4) { |entry| @entries.push entry.take( 3 ) }
 
-    puts "separateDigits"
-    p digits
-    # return digits
+    @entries
   end
 
-  def matchDigit( digit )
-    digit.join('\n')
+  def rawEntriesChunkedByDigit
+    rawEntries.map { |entry| separateDigits( entry ) }
   end
+
+  def parsedEntries
+    rawEntriesChunkedByDigit.map { |entry|
+      entry.map { |digitAs3x3Matrix|
+        LookupNumberFor[ digitAs3x3Matrix.join ] || "?" }.join }
+  end
+
+  private
+    def separateDigits( entryLines )
+      entryLines
+        .map { |line; split_line|
+          split_line = []
+          # puts "line"
+          # p line
+          line.split('').each_slice( 3 ) { | digitLine |
+            split_line.push( digitLine.join ) }
+          # puts "split_line"
+          # p split_line
+          split_line }
+        .transpose()
+    end
+
 end
--- spec/bank_ocr_spec.rb
+++ spec/bank_ocr_spec.rb
@@ -1,32 +1,11 @@
-FIXTURE_DIR = "/Users/clov3r/Dev/bank_ocr_kata/fixtures/"
-
-class BankOcrTestClient
-  include BankOcr
-end
-
 describe BankOcr do
-  @bankOcr = nil
-  before(:all) do
-    @bankOcr = BankOcrTestClient.new
-    @entries =
-      @bankOcr.readEntriesFromFile( fixture( 'use_case_1_in' ) )
-  end
 
-  it "is a module" do
-    expect(BankOcr).to be_a Module
-  end
+  it "parses entries into a string of digits" do
+    @bankOcr = BankOcr.new fixture( 'use_case_1_in' )
 
-  it "splits a file into entries" do
-            expect( @entries ).to be_an Array
-     expect( @entries.length ).to be 11
-  end
+    expect( @bankOcr.parsedEntries.join("\n") )
+        .to eq fixture( 'use_case_1_out' ).chomp
 
-  it "parses an entry into a string of digits" do
-
-    (0..9).each { |entry|
-      expect( @entries[entry] )
-        .to eq ( (1..9).collect{ entry }.join ) }
-
-    expect( @entries[10] ).to eq ( "123456789")
+    expect( @bankOcr.parsedEntries[10] ).to eq ( "123456789" )
   end
 end
